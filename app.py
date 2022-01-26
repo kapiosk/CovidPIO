25cc2905d9b4921ec87c4cdfc3aabb49548c220a
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-01-26 15:42:17 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+#!venv/bin python3
+# -*- coding: utf-8 -*-
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+baseUrl = 'https://www.pio.gov.cy/coronavirus/categories/press'
+html = urlopen(baseUrl).read()
+html = html.decode('utf-8')
+
+soup = BeautifulSoup(html, features='html.parser')
+pdfs = soup.find_all("a", {"class": "flag pdf"})
+
+with open('latest.html', 'w') as f:
+	for pdf in pdfs[:10]:
+		href = pdf["href"]
+		if "stay" in href or "rapid" in href:
+			f.writelines(str(pdf))
+
