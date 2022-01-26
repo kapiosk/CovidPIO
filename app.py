fd830e98cb835a86a6334ecb8bbadc54c12a1a7f
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-01-26 16:46:22 +0200

    update

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ html = html.decode('utf-8')
 soup = BeautifulSoup(html, features='html.parser')
 pdfs = soup.find_all("a", {"class": "flag pdf"})
 
-with open('index.html', 'w') as f:
+with open('latest.html', 'w') as f:
 	for pdf in pdfs[:10]:
 		href = pdf["href"]
 		if "stay" in href or "rapid" in href:
 			f.writelines(str(pdf))
+			f.writelines("<hr>")
 
