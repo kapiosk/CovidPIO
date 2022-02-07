2922bfeeee2d00b46c0f65857c1d9cbd5d4e0dc7
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-02-08 01:43:56 +0200

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
 
 baseUrl = 'https://www.pio.gov.cy/coronavirus/categories/press'
 html = urlopen(baseUrl).read()
@@ -10,6 +11,13 @@ html = html.decode('utf-8')
 
 soup = BeautifulSoup(html, features='html.parser')
 pdfs = soup.find_all("a", {"class": "flag pdf"})
+
+with open('data.csv', 'w', newline='') as csvfile:
+	csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for pdf in pdfs[:10]:
+                href = pdf["href"]
+                if "stay" in href or "rapid" in href:
+			csvwriter.writerow(href, pdf.text)
 
 with open('latest.html', 'w') as f:
 	for pdf in pdfs[:10]:
