6df54af3867eb43c60d65135b5c62aed2aea321f
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-02-08 01:45:02 +0200

    update

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ pdfs = soup.find_all("a", {"class": "flag pdf"})
 
 with open('data.csv', 'w', newline='') as csvfile:
 	csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for pdf in pdfs[:10]:
-                href = pdf["href"]
-                if "stay" in href or "rapid" in href:
+	for pdf in pdfs[:10]:
+		href = pdf["href"]
+		if "stay" in href or "rapid" in href:
 			csvwriter.writerow(href, pdf.text)
 
 with open('latest.html', 'w') as f:
